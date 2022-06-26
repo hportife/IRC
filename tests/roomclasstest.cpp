0f1a75c86dff27b639999829d4aa09c5f5c9a165
Horas Portifer <hportife@va-c3.kzn.21-school.ru>
2022-06-26 14:38:03 +0300

    start room test write

---
+++ tests/roomclasstest.cpp
@@ -0,0 +1,7 @@
+#include "../tools/LogIdentifier.hpp"
+#include "../srv_inside/Room.hpp"
+
+int main(void){
+    Room *room1, room2, room3, room4;
+    room1 = new Room("salamander", ROOM_MODE_PRIVATE, "salamander_room");
+}
