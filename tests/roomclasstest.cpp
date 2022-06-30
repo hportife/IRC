8eb3c38b376d86d61596597f6dc98ff6ab61e5e5
hportife <hportife@student.21-school.ru>
2022-06-30 19:21:17 +0300

    continue

--- tests/roomclasstest.cpp
+++ tests/roomclasstest.cpp
@@ -2,6 +2,14 @@
 #include "../srv_inside/Room.hpp"
 
 int main(void){
-    Room *room1, room2, room3, room4;
+    Room *room1, *room2, *room3, *room4;
     room1 = new Room("salamander", ROOM_MODE_PRIVATE, "salamander_room");
+    room2 = new Room("vik", ROOM_MODE_PRIVATE, "vikRoom");
+    room3 = new Room("yyyyyy", ROOM_MODE_PUBLIC, "1111122221231146134623462546235123462346134625734746");
+    room4 = new Room("121212", ROOM_MODE_PUBLIC, "ola");
+
+    room1->get_room_name();
+    room2->get_room_name();
+    room3->get_room_name();
+    room4->get_room_name();
 }
