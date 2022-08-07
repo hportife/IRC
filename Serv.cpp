b64e101bfd87d0ae123e89fe3f2329b20891bbb5
hportife <hportife@student.21-school.ru>
2022-08-07 08:08:02 +0300

    create Serv Class

--- Serv.cpp
+++ Serv.cpp
@@ -1,2 +1,32 @@
-
+#include "tools/LogIdentifier.hpp"
 #include "Serv.hpp"
+
+Serv::Serv() {
+    this->general_room_storage = new RoomStorage();
+    this->general_nickname_storage = new NicknameStorage();
+    this->general_user_storage = new UserStorage(
+            general_nickname_storage);
+    this->worked_flag = FLG_SRV_WORK;
+    std::cout << LogIdentifier::info(SERVERCLASS_)
+                << "Server class is start" << std::endl;
+}
+Serv::~Serv(){//need to add clear
+     }
+RoomStorage                 *Serv::getRoomStorage()const{
+    return (this->general_room_storage);
+}
+UserStorage                 *Serv::getUserStorage()const{
+    return (this->general_user_storage);
+}
+NicknameStorage             *Serv::getNicknameStorage()const{
+    return
+}
+std::queue<CommandLine *>   *Serv::GetTasks()const{
+    return (&this->tasks);
+}
+std::vector<pollfd>	        *Serv::GetPolls()const{
+    return (&this->general_polls);
+}
+void Serv::shutDown(){
+    this->worked_flag = FLG_SRV_DOWN;
+}
--- Serv.hpp
+++ Serv.hpp
@@ -1,8 +1,12 @@
 #ifndef IRC_SERV_HPP
 #define IRC_SERV_HPP
 
+#define FLG_SRV_WORK 2000
+#define FLG_SRV_DOWN 2010
+
 #include <queue>
 #include <iostream>
+#include <sys/poll.h>
 
 #include "srv_inside/Room.hpp"
 #include "srv_inside/RoomCreator.hpp"
@@ -18,10 +22,18 @@ private:
     UserStorage                 *general_user_storage;
     NicknameStorage             *general_nickname_storage;
     std::queue<CommandLine *>   tasks;
+    std::vector<pollfd>	        general_polls;
+    int                         worked_flag;
 
 public:
-    Serv();
+    Serv();//ok
     ~Serv();
+    RoomStorage                 *getRoomStorage()const;//ok
+    UserStorage                 *getUserStorage()const;//ok
+    NicknameStorage             *getNicknameStorage()const;//ok
+    std::queue<CommandLine *>   *GetTasks()const;//ok
+    std::vector<pollfd>	        *GetPolls()const;//ok
+    void                        shutDown();//ok
 };
 
 
