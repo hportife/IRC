af5e737e29445ff52ceb82295225019ad17aea9c
hportife <hportife@student.21-school.ru>
2022-08-07 13:18:03 +0300

    create commando

---
+++ commando.cpp
@@ -0,0 +1,5 @@
+//
+// Created by grafsky on 8/7/22.
+//
+
+#include "commando.hpp"
---
+++ commando.hpp
@@ -0,0 +1,14 @@
+//
+// Created by grafsky on 8/7/22.
+//
+
+#ifndef IRC_COMMANDO_HPP
+#define IRC_COMMANDO_HPP
+
+
+class commando {
+
+};
+
+
+#endif //IRC_COMMANDO_HPP
