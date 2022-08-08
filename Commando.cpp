01bd1c93781e039511e1f73ca7e66ade50030d9b
hportife <hportife@student.21-school.ru>
2022-08-08 07:24:23 +0300

    continue

--- Commando.cpp
+++ Commando.cpp
@@ -17,16 +17,8 @@ void Commando::getNextCommandLine() {
 int Commando::swapNickname(std::string old_nickname,
                            std::string new_nickname) {
     //ПРОВЕРКА ВАЛИДНОСТИ НИКА
-    if (new_nickname.length() > 9 || new_nickname.empty()) {
-        return (NICKNAME_IS_WRONG);
-    }
-    //ПРОВЕРКА НАЛИЧИЯ КОНФЛИКТОВ
-    if (this->general_serv
-            ->getNicknameStorage()
-            ->search_a_conflict(new_nickname)
-            == ERR_NICKNAMEINUSE) {
-        return (ERR_NICKNAMEINUSE);
-    }
+    if (nickname_validator(new_nickname) != NICKNAME_HAS_VALID)
+        return (nickname_validator(new_nickname));
     //ВЫДАЧА НИКНЕЙМА
     this->general_serv
             ->getNicknameStorage()
@@ -51,4 +43,35 @@ int Commando::setAwayMsg(std::string user_nickname) {
     this->general_serv->getUserStorage()
             ->search_by_nickname(user_nickname)
             ->setAwayMsg();
+}
+
+int Commando::userCommand(std::string nickname, int id,
+                          std::string realname,
+                          std::string clientname){
+    if (nickname_validator(nickname) != NICKNAME_HAS_VALID)
+        return (nickname_validator(nickname));
+    this->general_serv
+        ->getNicknameStorage()
+        ->add_nickname(nickname);
+    User *new_user = new User(nickname, id, realname, clientname);
+    this->general_serv
+        ->getUserStorage()
+        ->add_user(new_user->clone());
+    delete new_user;
+    return (NEW_USER_CREATE);
+}
+
+//-----------------------------validators---------------------------------------
+
+int Commando::nickname_validator(std::string nickname) {
+    if (nickname.length() > 9 || nickname.empty()) {
+        return (NICKNAME_IS_WRONG);
+    }
+    if (this->general_serv
+                ->getNicknameStorage()
+                ->search_a_conflict(nickname)
+        == ERR_NICKNAMEINUSE) {
+        return (ERR_NICKNAMEINUSE);
+    }
+    return (NICKNAME_HAS_VALID);
 }
--- Commando.hpp
+++ Commando.hpp
@@ -2,7 +2,9 @@
 #define IRC_COMMANDO_HPP
 
 #define NICKNAME_IS_WRONG   113
-#define NICKNAME_IS_GIVEN    114
+#define NICKNAME_IS_GIVEN   114
+#define NICKNAME_HAS_VALID  115
+#define NEW_USER_CREATE     116
 
 #include "srv_inside/CommandLine.hpp"
 #include "Serv.hpp"
@@ -12,11 +14,11 @@
 //PASS          -
 //NICK (1Arg)   -
 //NICK (2Arg)   - OK
-//USER          -
+//USER          - OK
 //OPER          -
 //QUIT          -
 //PRIVMSG       -
-//AWAY          - OK
+//AWAY(on/off)  - OK
 //NOTICE        -
 //WHO           -
 //WHOIS         -
@@ -50,11 +52,17 @@ public:
     Commando(Serv *server_class);
     ~Commando();
 
-    void    getNextCommandLine();//OK
-    int     executeActualCommand();//NOK
-    int     swapNickname(std::string old_nickname, std::string new_nickname);//OK
-    int     setAwayMsg(std::string user_nickname);//Ok
-    int     setAwayMsg(std::string user_nickname, std::string message);//ok
+    void    getNextCommandLine();                                           //OK
+    int     executeActualCommand();                                         //NOK
+    int     swapNickname(std::string old_nickname, std::string new_nickname);//OK - NICK (2Arg)
+    int     setAwayMsg(std::string user_nickname);                          //Ok - AWAY(off)
+    int     setAwayMsg(std::string user_nickname, std::string message);     //ok - AWAY(on)
+    int     userCommand(std::string nickname, int id,
+                        std::string clientname,
+                        std::string realname);                              //ok - USER
+
+    //-----------------------------validators-----------------------------
+    int     nickname_validator(std::string nickname);                       //ok
 };
 
 
