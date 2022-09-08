e315ac86ae7fd4b59349a0853784657f22142f38
hportife <hportife@student.21-school.ru>
2022-09-08 06:44:27 +0300

    add compiler command (in mainCircuit)

--- MainCircuit.cpp
+++ MainCircuit.cpp
@@ -2,7 +2,7 @@
 #include "includes/Serv.hpp"
 #include "includes/Connect.hpp"
 
-//c++ -std=c++98 ./srcs_connect/Connect.cpp MainCircuit.cpp Serv.cpp ./srv_inside/RoomStorage.cpp ./srv_inside/Room.cpp ./srv_inside/User.cpp ./srv_inside/UserStorage.cpp
+//c++ -std=c++98 ./srcs_connect/Connect.cpp MainCircuit.cpp Serv.cpp ./srv_inside/*.cpp tools/LogIdentifier.cpp Commando.cpp
 
 int main(int arc, char **arg){
     if (arc != 3) {
